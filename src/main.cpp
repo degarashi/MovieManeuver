fa047abffac26b4d03c8d5cb6f217780b1ed9c14
Degarashi <degarasu@gmail.com>
2022-09-24 22:38:44 +0900

    [clean] 不要なコードを削除

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,5 @@ int main(int argc, char *argv[]) {
     MainWindow w;
 	w.setFixedSize(640, 440);
 	w.show();
-
-	//QObject::connect(&test, SLOT())
-	// HWND hw = reinterpret_cast<HWND>(w.winId());
-	// SetWindowPos(hw, nullptr, 0, 0, 0, 0, SWP_NOSIZE);
     return a.exec();
 }
