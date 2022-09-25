719c5a8fc8761f38bbb16a147130cd9edb81ff42
Degarashi <degarasu@gmail.com>
2022-09-25 15:39:09 +0900

    [add] Wiimote用のInputManagerクラス

---
+++ src/inputmgr_wii.cpp
@@ -0,0 +1,44 @@
+#include "inputmgr_wii.hpp"
+#include "diag_wiimote.h"
+#include <wiiremote.h>
+
+namespace dg::wii {
+	Manager::Manager() {
+		WRMT_WiiRemote* wiiremotes[WRMT_MAX_DEVICES];
+		if(WRMT_Init() != 0)
+			throw Init_Fail("WRMT_Init() failed");
+		const int num = WRMT_GetNumWiiRemote();
+		qDebug() << num << " wii remotes found";
+
+		for(int i=0 ; i<num ; i++) {
+			auto* rmt = WRMT_GetWiiRemoteAt(i);
+			_remote.emplace_back(rmt);
+		}
+	}
+	Manager::~Manager() {
+		WRMT_Quit();
+	}
+	void Manager::onTimer() {
+		int idx;
+		while((idx = WRMT_Poll()) >= 0) {
+			// とりあえず0番以外は対応しない
+			if(idx != 0)
+				continue;
+
+			const auto press = _remote[idx].getPressedButton();
+			VKInputs vk;
+			for(int i=0 ; i<static_cast<int>(Button::_Num) ; i++) {
+				if(press[i]) {
+					vk.emplace_back(ButtonToVKMap.at(Button(i)));
+				}
+			}
+			emit onInput(vk);
+			emit onInputWii(_remote[idx]);
+		}
+	}
+	QWidget* Manager::makeDialog() {
+		auto* diag = new Diag_Wiimote();
+		connect(this, &Manager::onInputWii, diag, &Diag_Wiimote::updateDebugView);
+		return diag;
+	}
+}
---
+++ src/inputmgr_wii.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include "inputmgr.hpp"
+#include "wii_padstate.hpp"
+
+namespace dg::wii {
+	class Manager : public InputMgrBase {
+		Q_OBJECT
+		private:
+			using Remote_v = std::vector<Remote>;
+			Remote_v	_remote;
+
+		public:
+			Manager();
+			~Manager();
+			QWidget* makeDialog() override;
+		private slots:
+			void onTimer() override;
+		signals:
+			void onInputWii(const dg::wii::Remote& remote);
+	};
+}
