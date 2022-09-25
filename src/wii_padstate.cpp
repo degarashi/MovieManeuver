2666fe2f5508eb214c731ac3214216226beac15f
Degarashi <degarasu@gmail.com>
2022-09-25 15:34:53 +0900

    [add] Wiimote用のPadStateクラス

---
+++ src/wii_padstate.cpp
@@ -0,0 +1,68 @@
+#include "wii_padstate.hpp"
+#include <QtGlobal>
+#include <wiiremote.h>
+
+namespace dg {
+	namespace wii {
+		const ButtonToVKMap_t ButtonToVKMap = {
+			{Button::A, VirtualKey::A},
+			{Button::B, VirtualKey::B},
+			{Button::One, VirtualKey::X},
+			{Button::Two, VirtualKey::Y},
+			{Button::Minus, VirtualKey::Select},
+			{Button::Home, VirtualKey::L1},
+			{Button::Plus, VirtualKey::Start},
+			{Button::Left, VirtualKey::DLeft},
+			{Button::Up, VirtualKey::DUp},
+			{Button::Right, VirtualKey::DRight},
+			{Button::Down, VirtualKey::DDown},
+		};
+
+		Remote::Remote(Remote&& rmt):
+			_data(rmt._data),
+			_bstate(rmt._bstate)
+		{
+			rmt._data = nullptr;
+		}
+		Remote::Remote(WRMT_WiiRemote* data):
+			_data(data)
+		{
+			Q_ASSERT(data);
+
+			const WRMT_IOReturn rc = WRMT_WiiRemote_Open(data);
+			if(rc == WRMT_IO_ERROR)
+				throw Exception("WRMT_WiiRemote_Open() Failed");
+
+			// 余計な機能をOFF
+			WRMT_WiiRemote_SetEnabled(_data, WRMT_FUNCTION_CONTINUOUS, 0);
+			WRMT_WiiRemote_SetEnabled(_data, WRMT_FUNCTION_MOTION, 0);
+			WRMT_WiiRemote_SetEnabled(_data, WRMT_FUNCTION_IR, 0);
+			WRMT_WiiRemote_SetEnabled(_data, WRMT_FUNCTION_SPEAKER, 0);
+			WRMT_WiiRemote_Update(_data);
+		}
+		Remote::~Remote() {
+			WRMT_WiiRemote_Close(_data);
+		}
+		ButtonAr Remote::getPressedButton() const {
+			ButtonAr ret = {};
+			const int bs = WRMT_WiiRemote_GetState(_data, WRMT_DATA_BUTTONS);
+			for(auto& msk : Mask2B) {
+				ret[static_cast<int>(msk.second)] = (bs & msk.first);
+			}
+			return ret;
+		}
+		const Remote::MaskToButtonPair Remote::Mask2B[static_cast<size_t>(Button::_Num)] = {
+			{WRMT_MASK_BUTTON_A, Button::A},
+			{WRMT_MASK_BUTTON_B, Button::B},
+			{WRMT_MASK_BUTTON_ONE, Button::One},
+			{WRMT_MASK_BUTTON_TWO, Button::Two},
+			{WRMT_MASK_BUTTON_MINUS, Button::Minus},
+			{WRMT_MASK_BUTTON_PLUS, Button::Plus},
+			{WRMT_MASK_BUTTON_HOME, Button::Home},
+			{WRMT_MASK_BUTTON_UP, Button::Up},
+			{WRMT_MASK_BUTTON_LEFT, Button::Left},
+			{WRMT_MASK_BUTTON_RIGHT, Button::Right},
+			{WRMT_MASK_BUTTON_DOWN, Button::Down},
+		};
+	}
+}
---
+++ src/wii_padstate.hpp
@@ -0,0 +1,43 @@
+#pragma once
+#include "virtual_key.hpp"
+#include "buttonstate.hpp"
+#include <stdexcept>
+#include <unordered_map>
+
+struct wrmt_wiiremote;
+using WRMT_WiiRemote = wrmt_wiiremote;
+namespace dg::wii {
+	class Exception : public std::runtime_error {
+		using runtime_error::runtime_error;
+	};
+	class Init_Fail : public Exception {
+		using Exception::Exception;
+	};
+	enum class Button {
+		A, B,
+		One, Two,
+		Minus, Home, Plus,
+		Left, Up, Right, Down,
+		_Num
+	};
+	using ButtonToVKMap_t = std::unordered_map<Button, VirtualKey>;
+	extern const ButtonToVKMap_t ButtonToVKMap;
+	using ButtonAr = std::array<bool, static_cast<size_t>(Button::_Num)>;
+
+	// WiiMote用のPadState
+	class Remote {
+		private:
+			WRMT_WiiRemote*	_data;
+			using ButtonState_a = std::array<ButtonState, static_cast<size_t>(Button::_Num)>;
+			ButtonState_a	_bstate;
+			using MaskToButtonPair = std::pair<uint16_t, Button>;
+			const static MaskToButtonPair Mask2B[static_cast<size_t>(Button::_Num)];
+
+		public:
+			Remote(Remote&& rmt);
+			Remote(const Remote&) = delete;
+			Remote(WRMT_WiiRemote* data);
+			~Remote();
+			ButtonAr getPressedButton() const;
+	};
+}
