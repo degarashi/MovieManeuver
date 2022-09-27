b08b4152e15f305ffb7fc02f5717c9722146489c
Degarashi <degarasu@gmail.com>
2022-09-27 23:07:33 +0900

    [update] Manip_Udemy: struct -> class

--- src/manip_udemy.hpp
+++ src/manip_udemy.hpp
@@ -2,24 +2,25 @@
 #include "manip.hpp"
 
 namespace dg {
-	struct Manip_Udemy : Manip {
-		static void UProc(HWND hw, const Proc_t& p);
+	class Manip_Udemy : public Manip {
+		private:
+			static void UProc(HWND hw, const Proc_t& p);
+		public:
+			void startPause(HWND hw) const override;
+			void forward_5sec(HWND hw) const override;
+			void backward_5sec(HWND hw) const override;
+			void forward_10sec(HWND hw) const override;
+			void backward_10sec(HWND hw) const override;
+			void speedDown(HWND hw) const override;
+			void speedUp(HWND hw) const override;
+			void volumeDown(HWND hw) const override;
+			void volumeUp(HWND hw) const override;
+			void volumeMute(HWND hw) const override;
+			void fullScreen(HWND hw) const override;
 
-		void startPause(HWND hw) const override;
-		void forward_5sec(HWND hw) const override;
-		void backward_5sec(HWND hw) const override;
-		void forward_10sec(HWND hw) const override;
-		void backward_10sec(HWND hw) const override;
-		void speedDown(HWND hw) const override;
-		void speedUp(HWND hw) const override;
-		void volumeDown(HWND hw) const override;
-		void volumeUp(HWND hw) const override;
-		void volumeMute(HWND hw) const override;
-		void fullScreen(HWND hw) const override;
+			const std::wstring& getWindowTopName() const override;
+			const std::wstring& getWindowChildName() const override;
 
-		const std::wstring& getWindowTopName() const override;
-		const std::wstring& getWindowChildName() const override;
-
-		const QString& getName() const override;
+			const QString& getName() const override;
 	};
 }
