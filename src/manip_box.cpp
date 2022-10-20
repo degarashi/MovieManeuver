589e14dc559aab3e515739377a5b341f6c4f8a16
Degarashi <degarasu@gmail.com>
2022-10-20 21:05:04 +0900

    [add] Box.comのマニピュレータ

---
+++ src/manip_box.cpp
@@ -0,0 +1,76 @@
+#include "manip_box.hpp"
+#include "winop.hpp"
+#include <QThread>
+
+namespace dg {
+	void Manip_Box::_Focus(HWND hw, const Proc_t& p) {
+		TempSwitch(hw, [hw, &p](){
+			ClickLeftTop(hw, false, 192);
+			QThread::msleep(20);
+			p();
+		});
+	}
+	void Manip_Box::startPause(const HWND hw) const {
+		_Focus(hw, [](){
+			TapKey(VK_SPACE);
+		});
+	}
+	void Manip_Box::forward_5sec(const HWND hw) const {
+		_Focus(hw, [](){
+			TapKey(VK_RIGHT);
+		});
+	}
+	void Manip_Box::backward_5sec(const HWND hw) const {
+		_Focus(hw, [](){
+			TapKey(VK_LEFT);
+		});
+	}
+	void Manip_Box::forward_10sec(const HWND hw) const {
+		_Focus(hw, [](){
+			TapKey(u8'L');
+		});
+	}
+	void Manip_Box::backward_10sec(const HWND hw) const {
+		_Focus(hw, [](){
+			TapKey(u8'J');
+		});
+	}
+	void Manip_Box::speedDown(const HWND hw) const {
+		_Focus(hw, [](){
+			TapKey(0xBC, VK_SHIFT);
+		});
+	}
+	void Manip_Box::speedUp(const HWND hw) const {
+		_Focus(hw, [](){
+			TapKey(0xBE, VK_SHIFT);
+		});
+	}
+	void Manip_Box::volumeDown(const HWND hw) const {
+		_Focus(hw, [](){
+			TapKey(VK_DOWN);
+		});
+	}
+	void Manip_Box::volumeUp(const HWND hw) const {
+		_Focus(hw, [](){
+			TapKey(VK_UP);
+		});
+	}
+	void Manip_Box::volumeMute(const HWND hw) const {
+		_Focus(hw, [](){
+			TapKey(u8'M');
+		});
+	}
+
+	const std::wstring& Manip_Box::getWindowTopName() const {
+		static std::wstring ret(L"Powered by Box");
+		return ret;
+	}
+	const std::wstring& Manip_Box::getWindowChildName() const {
+		static std::wstring ret(L"Legacy Window");
+		return ret;
+	}
+	const QString& Manip_Box::getName() const {
+		static QString Name = QString::fromUtf8(u8"Box Manipulator");
+		return Name;
+	}
+}
---
+++ src/manip_box.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#include "manip.hpp"
+
+namespace dg {
+	class Manip_Box : public Manip {
+		private:
+			// 動画ウィンドウにフォーカスする処理
+			static void _Focus(HWND hw, const Proc_t& p);
+		public:
+			void startPause(HWND hw) const override;
+			void forward_5sec(HWND hw) const override;
+			void backward_5sec(HWND hw) const override;
+			void forward_10sec(HWND hw) const override;
+			void backward_10sec(HWND hw) const override;
+			void speedDown(HWND hw) const override;
+			void speedUp(HWND hw) const override;
+			void volumeDown(HWND hw) const override;
+			void volumeUp(HWND hw) const override;
+			void volumeMute(HWND hw) const override;
+
+			const std::wstring& getWindowTopName() const override;
+			const std::wstring& getWindowChildName() const override;
+
+			const QString& getName() const override;
+	};
+}
