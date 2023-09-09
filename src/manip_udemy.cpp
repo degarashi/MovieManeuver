a1a26ad100b7e7a7e8c0b01f08332cfea10acc52
Degarashi <degarasu@gmail.com>
2023-09-09 21:33:00 +0900

    [update] スペースをタブに統一

--- src/manip_udemy.cpp
+++ src/manip_udemy.cpp
@@ -4,11 +4,11 @@
 
 namespace dg {
 	namespace {
-        constexpr unsigned long WAIT = 50;
+		constexpr unsigned long WAIT = 50;
 	}
-    void Manip_Udemy::setFocus(HWND hw) const {
-        ClickLeftTop(hw, true, true, 192);
-        QThread::msleep(20);
+	void Manip_Udemy::setFocus(HWND hw) const {
+		ClickLeftTop(hw, true, true, 192);
+		QThread::msleep(20);
 	}
 	const std::wstring& Manip_Udemy::getWindowTopName() const {
 		static std::wstring ret(L"Udemy");
@@ -19,45 +19,45 @@ namespace dg {
 		return ret;
 	}
 	void Manip_Udemy::startPause(const HWND hw) const {
-        TapKey(VK_SPACE);
-    }
+		TapKey(VK_SPACE);
+	}
 	void Manip_Udemy::forward_few(const HWND hw) const {
-        TapKey(VK_RIGHT);
-    }
+		TapKey(VK_RIGHT);
+	}
 	void Manip_Udemy::backward_few(const HWND hw) const {
-        TapKey(VK_LEFT);
-    }
+		TapKey(VK_LEFT);
+	}
 	void Manip_Udemy::forward_medium(const HWND hw) const {
-        TapKey(VK_RIGHT);
-        QThread::msleep(WAIT);
-        TapKey(VK_RIGHT);
-    }
+		TapKey(VK_RIGHT);
+		QThread::msleep(WAIT);
+		TapKey(VK_RIGHT);
+	}
 	void Manip_Udemy::backward_medium(const HWND hw) const {
-        TapKey(VK_LEFT);
-        QThread::msleep(WAIT);
-        TapKey(VK_LEFT);
-    }
+		TapKey(VK_LEFT);
+		QThread::msleep(WAIT);
+		TapKey(VK_LEFT);
+	}
 	void Manip_Udemy::speedDown(const HWND hw) const {
-        TapKey(VK_LEFT, VK_SHIFT);
-    }
+		TapKey(VK_LEFT, VK_SHIFT);
+	}
 	void Manip_Udemy::speedUp(const HWND hw) const {
-        TapKey(VK_RIGHT, VK_SHIFT);
-    }
+		TapKey(VK_RIGHT, VK_SHIFT);
+	}
 	void Manip_Udemy::volumeDown(const HWND hw) const {
-        TapKey(VK_DOWN);
-    }
+		TapKey(VK_DOWN);
+	}
 	void Manip_Udemy::volumeUp(const HWND hw) const {
-        TapKey(VK_UP);
-    }
+		TapKey(VK_UP);
+	}
 	void Manip_Udemy::volumeMute(const HWND hw) const {
-        TapKey(u8'M');
-    }
+		TapKey(u8'M');
+	}
 	void Manip_Udemy::fullScreen(const HWND hw) const {
-        TapKey(u8'F');
-    }
+		TapKey(u8'F');
+	}
 	void Manip_Udemy::captionSwitch(const HWND hw) const {
-        TapKey(u8'C');
-    }
+		TapKey(u8'C');
+	}
 	const QString& Manip_Udemy::getName() const {
 		static QString Name = QString::fromUtf8(u8"Udemy Manipulator");
 		return Name;
