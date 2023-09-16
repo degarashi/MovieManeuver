3a4d16eaedc96d37cdc6daa799c2048b4e65c541
Degarashi <degarasu@gmail.com>
2023-09-16 13:29:09 +0900

    [update] Vector3 定義

---
+++ src/axisstate_n.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include <cstddef>
+#include "vector.hpp"
+
+namespace dg {
+	template <size_t N>
+	class AxisState {
+		private:
+	};
+}
--- src/vector.hpp
+++ src/vector.hpp
@@ -15,7 +15,18 @@ namespace dg {
 			std::array<T,2>		ar;
 		};
 	};
-
 	using Vec2 = VecBase<float, 2>;
 	using IVec2 = VecBase<int, 2>;
+
+	template <class T>
+	struct VecBase<T, 3> {
+		union {
+			struct {
+				T	x, y, z;
+			};
+			std::array<T,3>		ar;
+		};
+	};
+	using Vec3 = VecBase<float, 3>;
+	using IVec3 = VecBase<int, 3>;
 }
