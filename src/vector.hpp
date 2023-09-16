9e5a806be50c720665444287adbaa9af5d5e4621
Degarashi <degarasu@gmail.com>
2023-09-16 13:23:38 +0900

    [update] VecBase: テンプレート使用の記述

--- src/vector.hpp
+++ src/vector.hpp
@@ -1,10 +1,21 @@
 ﻿#pragma once
+#include <cstddef>
+#include <array>
 
 namespace dg {
-    template <class T>
-    struct VecBase {
-        T x, y;
-    };
-    using Vec2 = VecBase<float>;
-    using IVec2 = VecBase<int>;
+	template <class T, size_t N>
+	struct VecBase;
+
+	template <class T>
+	struct VecBase<T, 2> {
+		union {
+			struct {
+				T	x, y;
+			};
+			std::array<T,2>		ar;
+		};
+	};
+
+	using Vec2 = VecBase<float, 2>;
+	using IVec2 = VecBase<int, 2>;
 }
