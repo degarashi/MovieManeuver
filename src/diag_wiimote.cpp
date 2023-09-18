dd0ed57427096d9e78a69ef97c12a08f1eb28d6b
Degarashi <degarasu@gmail.com>
2023-09-18 14:17:23 +0900

    [update] wii_padstate: ダイアログに加速度センサーのボタン値(pressing)を表示

--- src/diag_wiimote.cpp
+++ src/diag_wiimote.cpp
@@ -22,5 +22,22 @@ namespace dg::wii {
 			const int idx = static_cast<int>(VKToButtonMap.at(k.key));
 			ar[idx]->setChecked(k.pressed);
 		}
+
+		const auto acc = remote.getAxisDiff();
+		std::array<std::pair<VirtualKey, QCheckBox *const>, 6> ar_acc {
+			std::make_pair(VirtualKey::TL_Left, _ui->cbAccX_Neg),
+			{VirtualKey::TL_Right, _ui->cbAccX_Pos},
+			{VirtualKey::TL_Up, _ui->cbAccY_Neg},
+			{VirtualKey::TL_Down, _ui->cbAccY_Pos},
+			{VirtualKey::TR_Left, _ui->cbAccZ_Neg},
+			{VirtualKey::TR_Right, _ui->cbAccZ_Pos},
+		};
+		for(auto& a : acc) {
+			auto itr = std::find_if(ar_acc.begin(), ar_acc.end(),
+								 [&a](auto& p){ return a.key == p.first; });
+			if(itr != ar_acc.end()) {
+				itr->second->setChecked(a.pressed);
+			}
+		}
 	}
 }
