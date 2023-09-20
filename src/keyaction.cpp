1d01c20b7d17691a9378c20bdf9249b7e045a007
Degarashi <degarasu@gmail.com>
2023-09-20 14:06:26 +0900

    [update] KeyAction: Act_MethodとAct_Funcを定義

--- src/keyaction.cpp
+++ src/keyaction.cpp
@@ -1,13 +1,16 @@
 #include "keyaction.hpp"
 
 namespace dg::input {
-	// --- Act_Method ---
-	Act_Method::Act_Method(ManipF method, bool bForeground):
-		  method(method), bForeground(bForeground){}
-
-	void Act_Method::action(ActionParam& ap) const {
+	// --- Act_Func ---
+	void Act_Func::action(ActionParam& ap) const {
 		if(bForeground)
 			ap.foreground();
-		ap.callManip(method);
+		func(ap);
 	}
+
+	// --- Act_Method ---
+	Act_Method::Act_Method(ManipF method, bool bForeground):
+		Act_Func([method](ActionParam& ap){
+			ap.callManip(method);
+		}, bForeground) {}
 }
--- src/keyaction.hpp
+++ src/keyaction.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "manip_if.hpp"
+#include <functional>
 #include <memory>
 
 namespace dg::input {
@@ -7,12 +8,20 @@ namespace dg::input {
 	struct Action {
 		virtual void action(ActionParam& ap) const = 0;
 	};
-	struct Act_Method : Action {
-		ManipF	method;
+	struct Act_Func : Action {
+		using Func = std::function<void (ActionParam&)>;
+		Func	func;
 		bool	bForeground;
 
-		Act_Method(ManipF method, bool bForeground);
+		template <class F>
+		Act_Func(F&& f, bool bForeground):
+			  func(std::forward<F>(f)),
+			  bForeground(bForeground) {}
 		void action(ActionParam& ap) const override;
+	};
+	struct Act_Method : Act_Func {
+		ManipF	method;
+		Act_Method(ManipF method, bool bForeground);
 	};
 	using Action_S = std::shared_ptr<Action>;
 }
