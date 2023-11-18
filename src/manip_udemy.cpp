8250db170ae6b088109e7dba8bdea51eae3ad604
Degarashi <degarasu@gmail.com>
2023-11-18 12:19:50 +0900

    [fix] Manip_Udemy: フォーカス処理で押すキーをEscapeからMenuキーに変更

--- src/manip_udemy.cpp
+++ src/manip_udemy.cpp
@@ -12,7 +12,7 @@ namespace dg {
 		ClickLeftTop(hw, true, true, 192);
 		QThread::msleep(20);
 		TapKey(VK_IME_OFF);
-		TapKey(VK_ESCAPE);
+		TapKey(VK_MENU);
 	}
 	const std::wstring& Manip_Udemy::getWindowTopName() const {
 		static std::wstring ret(L"Udemy");
